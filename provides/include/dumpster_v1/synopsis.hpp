923dce67531de671903c4e82f1dec2c5293e5d6c
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-04-20 17:11:34 +0300

    s/-/=/

--- provides/include/dumpster_v1/synopsis.hpp
+++ provides/include/dumpster_v1/synopsis.hpp
@@ -4,14 +4,14 @@
 
 namespace dumpster_v1 {
 
-// finally.hpp -----------------------------------------------------------------
+// finally.hpp =================================================================
 
 template <class Action> struct finally_t;
 
 template <class Action>
 finally_t<std::remove_cvref_t<Action>> finally(Action &&action);
 
-// insertion-sort.hpp ----------------------------------------------------------
+// insertion_sort.hpp ==========================================================
 
 template <class RandomIt, class Less, class IsSentinel>
 void insertion_sort(RandomIt values, Less less, IsSentinel is_sentinel);
