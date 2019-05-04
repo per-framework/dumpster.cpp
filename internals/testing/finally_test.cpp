c3ad24d70acd860a137387eac7403e768e77e081
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-05-05 00:23:10 +0300

    Allow function as finalizer

--- internals/testing/finally_test.cpp
+++ internals/testing/finally_test.cpp
@@ -4,7 +4,7 @@
 
 using namespace testing_v1;
 
-auto finally_test = test([]() {
+auto finally_lambda_test = test([]() {
   bool finalized = false;
   bool executed = false;
 
@@ -14,5 +14,20 @@ auto finally_test = test([]() {
   }
 
   verify(finalized);
+  verify(executed);
+});
+
+static bool s_finalized = false;
+static void s_function() { s_finalized = true; }
+
+auto finally_function_test = test([]() {
+  bool executed = false;
+
+  {
+    auto finalizer = dumpster_v1::finally(s_function);
+    executed = true;
+  }
+
+  verify(s_finalized);
   verify(executed);
 });
--- provides/include/dumpster_v1/finally.hpp
+++ provides/include/dumpster_v1/finally.hpp
@@ -7,14 +7,15 @@
 template <class Action> struct dumpster_v1::finally_t {
   ~finally_t() { m_action(); }
 
-  finally_t(Action &&action) : m_action(std::move(action)) {}
-  finally_t(const Action &action) : m_action(action) {}
+  template <class ForwardableAction>
+  finally_t(ForwardableAction &&action)
+      : m_action(std::forward<ForwardableAction>(action)) {}
 
   finally_t(const finally_t &) = delete;
   finally_t operator=(const finally_t &) = delete;
 
 private:
-  Action m_action;
+  std::conditional_t<std::is_function_v<Action>, Action *, Action> m_action;
 };
 
 template <class Action>
