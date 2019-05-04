48139b137b980d0780b2c7f1321f23f1604170d0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-05-05 00:32:05 +0300

    Doc comments

--- provides/include/dumpster_v1/synopsis.hpp
+++ provides/include/dumpster_v1/synopsis.hpp
@@ -6,13 +6,18 @@ namespace dumpster_v1 {
 
 // finally.hpp =================================================================
 
+/// Finalizer that invokes stored action when destroyed.
 template <class Action> struct finally_t;
 
+/// Creates a finalizer that invokes the action when destroyed.
 template <class Action>
 finally_t<std::remove_cvref_t<Action>> finally(Action &&action);
 
 // insertion_sort.hpp ==========================================================
 
+/// Sorts given sentinel terminated sequence to ascending order.  Intended for
+/// use cases where sequences are expected to be short, and low overhead and
+/// small code size are desired.
 template <class RandomIt, class Less, class IsSentinel>
 void insertion_sort(RandomIt values, Less less, IsSentinel is_sentinel);
 
