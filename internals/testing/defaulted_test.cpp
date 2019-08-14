95eb852fee6c13e207e379ecaf9e88bad0baa113
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-08-14 23:19:15 +0300

    Add `defaulted<Value, defaults...>` and `zeroed<Value>`

---
+++ internals/testing/defaulted_test.cpp
@@ -0,0 +1,26 @@
+#include "dumpster_v1/defaulted.hpp"
+
+#include "testing_v1/test.hpp"
+
+using namespace testing_v1;
+
+auto defaulted_test = test([]() {
+  {
+    dumpster_v1::defaulted<int, 42> x;
+    verify(x.value == 42);
+    x = 101;
+    verify(x == 101);
+  }
+  {
+    dumpster_v1::defaulted<short, 42> x(short(101));
+    verify(x == 101);
+  }
+  {
+    const dumpster_v1::zeroed<std::size_t> x;
+    verify(x == 0);
+  }
+  {
+    dumpster_v1::zeroed<char *> x;
+    verify(x == nullptr);
+  }
+});
